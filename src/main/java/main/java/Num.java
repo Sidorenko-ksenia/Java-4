27b48bdfa51870e263f4de094204395ab2b34575
Sidorenko-ksenia <ksiu99@mail.ru>
2020-09-23 20:58:54 +0300

    commit

---
+++ src/main/java/main/java/Num.java
@@ -0,0 +1,12 @@
+package main.java;
+import java.util.Scanner;
+public class Num {
+    public static void main(String[] args) {
+     Scanner scan = new Scanner(System.in);
+        int[] a = {1, 2, 3, 4, 5};
+        for (int i : a) {
+            System.out.println(i);
+            System.out.print(i);
+        }
+    }
+}
